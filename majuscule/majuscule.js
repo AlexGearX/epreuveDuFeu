d2569a7c305dc9099ea7a3a780ac0d5e7960cb57
AlexGearX <alexgear34@gmail.com>
2020-05-30 15:22:46 +0200

    test

--- majuscule/majuscule.js
+++ majuscule/majuscule.js
@@ -1,6 +1,6 @@
 var readline= require("readline-sync");
 var chars = readline.question("Entrez une phrase :").split('');
-var temp = 0
+var temp = 0;
 
 for (var index = 0; index < chars.length; index++) {
     if (chars[index] == ' ') {
