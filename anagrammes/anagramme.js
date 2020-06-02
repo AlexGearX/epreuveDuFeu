341e1472279ff1510a923ee3b3f1ee5b9771462e
AlexGearX <alexgear34@gmail.com>
2020-06-02 11:24:45 +0200

    Fin Anagramme

--- anagrammes/anagramme.js
+++ anagrammes/anagramme.js
@@ -1,27 +1,37 @@
 var readline= require("readline-sync");
 var fs = require("fs");
-var word = readline.question("Entrez un mot: ").split(' ');
+var word = readline.question("Entrez un mot: ");
 var wordList = fs.readFileSync('fr.txt').toString().split("\n"); 
 
-for (let index = 0; index < wordList.length; index++) {
-    function ana(word,worldlist,index)    
+var i = 0                                           //Initialise la variable i et le tableau result       
+var result = [] 
+while (i < wordList.length-1) {                     //Tant Que i < taille du tableau wordList -1
+    if (anagramSortSort (word, wordList[i])) {      //Si la fonction retourne Vrai alors :
+         result.push(wordList[i])                   //Ajoute la valeur de wordList[i] au tableau result (à la derniere case du tableau grace à push)
+         i += 1                                     //Incrementation de la variable i
+         if (i >= wordList.length-1) {              //Si i est >= à taille de wordList -1 (quand il est sur la derniere boucle) alors j'affiche le tableau result
+           console.log (result);  
+         }        
+    }else{                                          //Si la fonction retourne Faux alors :
+       i += 1 
+       if (i >= wordList.length-1) {
+            console.log (result);  
+        }
+    }
 }
 
-
-
-function ana(word,wordlist,index)
-if (word.length == wordlist[index].length) { 
-    for (let i = 0; i < word.length; i++) {
-        if (word[i] == wordList[i]) {
-            val += 1
-            if (val == word.length) {
-                 console.log(wordList[index]);
-            }
-        } else {
-            return false
-        }
-        
+function anagramSort (str1, str2) {                 
+    
+     if (str1.length !== str2.length) {             //Vérifie si les deux strings ont la même taille.
+        return false;
     }
-} else {
-    return false
+    
+
+    var s1 = str1.split('').sort().join('');        //Triez les deux strings.
+    var s2 = str2.split('').sort().join('');
+
+    
+    if (s1 === s2) {                                //Compare les deux strings triées.
+        return true
+    } 
 }
