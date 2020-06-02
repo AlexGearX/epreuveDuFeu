a87238e507232c3d5fd34f028622a575cbe8111e
AlexGearX <alexgear34@gmail.com>
2020-06-02 17:28:31 +0200

    Anagramme opti

--- anagrammes/anagramme.js
+++ anagrammes/anagramme.js
@@ -6,19 +6,12 @@ var wordList = fs.readFileSync('fr.txt').toString().split("\n");
 var i = 0                                           //Initialise la variable i et le tableau result       
 var result = [] 
 while (i < wordList.length-1) {                     //Tant Que i < taille du tableau wordList -1
-    if (anagramSortSort (word, wordList[i])) {      //Si la fonction retourne Vrai alors :
-         result.push(wordList[i])                   //Ajoute la valeur de wordList[i] au tableau result (à la derniere case du tableau grace à push)
-         i += 1                                     //Incrementation de la variable i
-         if (i >= wordList.length-1) {              //Si i est >= à taille de wordList -1 (quand il est sur la derniere boucle) alors j'affiche le tableau result
-           console.log (result);  
-         }        
-    }else{                                          //Si la fonction retourne Faux alors :
-       i += 1 
-       if (i >= wordList.length-1) {
-            console.log (result);  
-        }
+    if (anagramSort (word, wordList[i])) {          //Si la fonction retourne Vrai alors :
+         result.push(wordList[i])                   //Ajoute la valeur de wordList[i] au tableau result (à la derniere case du tableau grace à push)                                                  
     }
+i += 1                                              //Incrementation de la variable i
 }
+console.log (result);
 
 function anagramSort (str1, str2) {                 
     
@@ -26,11 +19,9 @@ function anagramSort (str1, str2) {
         return false;
     }
     
-
     var s1 = str1.split('').sort().join('');        //Triez les deux strings.
     var s2 = str2.split('').sort().join('');
 
-    
     if (s1 === s2) {                                //Compare les deux strings triées.
         return true
     } 
