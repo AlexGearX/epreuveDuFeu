0e5a81eac0ec93c330450ef2e16e92ccef0647bd
AlexGearX <alexgear34@gmail.com>
2020-06-07 13:42:12 +0200

    2nd factorial exercice

--- 8_factorielLeRetour/factoriel2.js
+++ 8_factorielLeRetour/factoriel2.js
@@ -1,12 +1,32 @@
-function nbfacto(nb){
-    if (nb == 1) {
-        return 1
-    } else {
-        return (nb * nbfacto(nb-1))
-    }
-}
-var readline= require("readline-sync");
-var nb = readline.questionInt("Entrez un nombre à factoriser: ")
-var nb = nb + n
 
-console.log (nbfacto(nb));
+function factorial(nbFact)
+{
+  if(nbFact === 0)
+    return '1'
+
+  if(!nbFact)
+    return ''
+
+  var i, nextNb, tmp,
+
+  result = nbFact.toString().split('').reverse().map(Number)
+
+  while(--nbFact){
+    i = 0
+    tmp = 0
+
+    while((nextNb = result[i++]) !== undefined || tmp) {
+      tmp = (nextNb || 0) * nbFact + tmp
+      result[i-1] = tmp % 10
+      tmp = parseInt(tmp/10)
+    }
+  }
+for (let j = 3; j < result.length; j+=4) {
+  result.splice((0+j), 0, [","]) 
+}
+  return result.reverse().join('')
+}
+
+var readline= require("readline-sync");
+var nbFact = readline.questionInt("Entrez un nombre à factoriser: ")
+console.log (factorial(nbFact));
